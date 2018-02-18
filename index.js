77a6d1b3eda49d75f481ddf83619f643cf6f60f4
dwrz <dwrz@dwrz.net>
2018-02-18 18:39:07 +0000

    Add promisify util

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const mail = require('nodemailer');
 const moment = require('moment');
+const { promisify } = require('util');
 const shell = require('shelljs');
 const twilio = require('twilio');
 
