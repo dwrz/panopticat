6fff857446bac5a6e241fdfd0924c8e3a7ab6f84
dwrz <dwrz@dwrz.net>
2018-03-04 03:20:56 +0000

    Add certificate and private key files to config
    Document commands to create self-signed certificate and key.

--- config.js
+++ config.js
@@ -7,8 +7,18 @@ const readFile = (file) => {
     return null;
   }
 };
+
+// openssl genrsa -out localhost.key 2048
+// openssl req -new -x509 -key localhost.key -out localhost.cert -days 3650 -subj /CN=localhost
+const cert = readFile('./panopticat.cert');
+const key = readFile('./panopticat.key');
+
 module.exports = () => {
   const config = {
+    credentials: {
+      cert,
+      key,
+    },
     port: 3000,
     secret: crypto.randomBytes(24).toString('hex'),
     snapInterval: '00 */30 * * * *',
