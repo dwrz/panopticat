01b91fdcd2688d734260a38c1465ee982820a01d
dwrz <dwrz@dwrz.net>
2018-03-04 03:20:01 +0000

    Add secret to config

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
 module.exports = () => {
   const config = {
     port: 3000,
+    secret: crypto.randomBytes(24).toString('hex'),
     snapInterval: '00 */30 * * * *',
   };
   return config;
