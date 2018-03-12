5df3a1721347c4697561f71149fabca995a2a41c
dwrz <dwrz@dwrz.net>
2018-03-12 02:19:49 +0000

    Make webpack config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const paths = {
+  public: path.resolve(__dirname, 'public'),
+  src: path.resolve(__dirname, 'src'),
+};
+
+module.exports = {
+  entry: path.join(paths.src, 'app.jsx'),
+  output: {
+    path: paths.public,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|jpe?g|png|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader?name=images/[name].[ext]',
+            options: { bypassOnDebug: true },
+          },
+        ],
+
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+
+      },
+      {
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff',
+
+      },
+    ],
+  },
+};
