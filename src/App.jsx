ff66bb7e8364a5be10898b78c7c4aea2129f0dbc
dwrz <dwrz@dwrz.net>
2018-04-10 01:52:43 +0000

    Add basic App component

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import 'bootstrap';
+import React from 'react';
+import { render } from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pw: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron">
+          <h1>Panopticat</h1>
+        </div>
+        <div>
+          <div className="form-group mx-sm-3 mb-2">
+            <label htmlFor="inputPassword2" className="sr-only">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Password" />
+          </div>
+          <button type="submit" className="btn btn-primary mb-2">Confirm identity</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
